6bcb5a955e3347ecbca8ef479a6f5670d3c2125b
LeeYoHan <yohan4551@gmail.com>
2023-07-16 16:47:38 +0900

    feat: add Board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import Board from "./components/board";
 
 function App() {
-
   return (
-    <div>
-      hello 
+    <div className="px-10 py-8">
+      <header>
+        <h1 className="text-3xl text-white font-bold">Proeject Kanvan</h1>
+      </header>
+      <main className="mt-10">
+        <section>
+          <Board />
+        </section>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/board/index.tsx
@@ -0,0 +1,20 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+
+const Board = () => (
+  <article className="w-80 px-6 py-4 rounded-lg bg-gray-200 flex flex-col gap-y-5">
+    <header>
+      <h3 className="text-lg font-semibold">To Do</h3>
+    </header>
+    <section>
+      <div className="p-3 bg-white rounded-md">Task</div>
+    </section>
+    <footer>
+      <button className="inline-flex items-center gap-x-2">
+        <PlusIcon className="w-5 h-5" />
+        <span className="font-medium">Add a card</span>
+      </button>
+    </footer>
+  </article>
+);
+
+export default Board;
